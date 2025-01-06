667b04e988d66ce170fd4f052e07708b4b28cd6c
nikos <nikgiannsm@gmail.com>
2025-01-06 15:37:28 +0200

    Added database connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import db from './db/dbConnection';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+db();
 
 app.use(cors());
 app.use(express.json());
