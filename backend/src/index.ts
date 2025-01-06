97eeafaecb25633e317feb8f4998c106f2328e31
nikos <nikgiannsm@gmail.com>
2025-01-06 17:09:48 +0200

    Organised be main folder and file structure.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,10 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import db from './db/dbConnection';
+import app from './app';
 
 dotenv.config();
-
-const app = express();
 const port = process.env.PORT || 3000;
-
 db();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/hello", (req, res) => {
-	res.json({ message: "Hello World!!" });
-});
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
