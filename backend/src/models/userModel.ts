30c6b3fb539c1338110d4d4bc558a4f343a0e969
nikos <nikgiannsm@gmail.com>
2025-01-06 18:16:15 +0200

    Added User admin and guest models

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import Document from 'mongoose';
+
+interface IAdmin extends Document{
+    name: String;
+    email: String;
+    passwordHash: String;
+    role: String;
+}
+
+
+const adminSchema = new mongoose.Schema<IAdmin>({
+    name: {type: String , required: true },
+    email: {type: String , required: true , unique: true},
+    passwordHash: {type: String , required: true },
+    role: {type: String , required: true }
+});
+
+interface IGuest extends Document {
+    email: String
+}
+
+const guestSchema = new mongoose.Schema<IGuest>({
+    email: {type: String , required: true , unique: true}
+});
+
+const Admin = mongoose.model<IAdmin>('Admin' , adminSchema);
+const Guest = mongoose.model<IGuest>('Guest' , guestSchema);
+
+export {Admin, Guest};
