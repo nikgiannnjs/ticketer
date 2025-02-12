8712b7e50fcd49b418603ed246dd79400d651934
nikos <nikgiannsm@gmail.com>
2025-02-12 13:32:49 +0200

    Fixed superAdminCheck middleware test

--- backend/src/test/middlewares/superAdminAccessOnly.test.ts
+++ backend/src/test/middlewares/superAdminAccessOnly.test.ts
@@ -1,54 +1,116 @@
-import { describe, it, expect } from "vitest";
-import app from "@/app";
-import request from "supertest";
+import { describe, it, expect, beforeEach, vi } from "vitest";
+import { Request, Response, NextFunction } from "express";
+import { superAdminCheck } from "@/middleware/superAdminAccessOnly";
 import jwt from "jsonwebtoken";
 import { Admin } from "@/models/userModel";
 
 process.env.JWT_SECRET = "test";
 const secret = process.env.JWT_SECRET;
 
-describe("superAdminCheck", async () => {
-  describe("POST /test-auth", async () => {
+vi.mock("@/models/userModel", () => ({
+  Admin: {
+    findOne: vi.fn(),
+  },
+}));
+
+describe("superAdmincheck middleware", () => {
+  let mockReq: Partial<Request>;
+  let mockRes: Partial<Response>;
+  let mockNextFunction: NextFunction;
+
+  beforeEach(() => {
+    mockReq = {
+      headers: {},
+    };
+    mockRes = {
+      status: vi.fn().mockReturnThis(),
+      json: vi.fn().mockReturnThis(),
+    };
+    mockNextFunction = vi.fn() as unknown as NextFunction;
+  });
+
+  describe("active", () => {
     it("should return 400 access denied if isSuperAdmin false", async () => {
-      const admin = new Admin({
+      vi.mocked(Admin.findOne).mockResolvedValue({
         email: "admin@test.com",
         status: "active",
       });
-      await admin.save();
 
-      const token = await jwt.sign({ email: "admin@test.com" }, secret, {
-        expiresIn: "1h",
-      });
-
-      const res = await request(app)
-        .post("/users/test-admin")
-        .set("Authorization", `Bearer ${token}`);
-
-      expect(res.status).toBe(400);
-      expect(res.body.message).toBe("Access denied.");
-    });
-
-    it("should return 200 if isSuperAdmin true", async () => {
-      const admin = new Admin({
-        email: "admin@test.com",
-        status: "super-admin",
-      });
-      await admin.save();
-
-      const token = await jwt.sign(
-        { email: "admin@test.com", isSuperAdmin: true },
-        secret,
-        {
-          expiresIn: "1h",
-        }
+      const token = jwt.sign(
+        { isSuperAdmin: false, email: "admin@test.com" },
+        secret
       );
 
-      const res = await request(app)
-        .post("/users/test-admin")
-        .set("Authorization", `Bearer ${token}`);
+      mockReq.headers = {
+        authorization: `Bearer ${token}`,
+      };
 
-      expect(res.status).toBe(200);
-      expect(res.body.message).toBe("Middleware passed.");
+      await superAdminCheck(
+        mockReq as Request,
+        mockRes as Response,
+        mockNextFunction
+      );
+
+      expect(mockNextFunction).not.toHaveBeenCalled();
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.json).toHaveBeenCalledWith({
+        message: "Access denied.",
+      });
+    });
+
+    describe("not found", () => {
+      it("should return 404 if user not found", async () => {
+        vi.mocked(Admin.findOne).mockResolvedValue(null);
+
+        const token = jwt.sign(
+          { isSuperAdmin: false, email: "wrongUser@test.com" },
+          secret
+        );
+
+        mockReq.headers = {
+          authorization: `Bearer ${token}`,
+        };
+
+        await superAdminCheck(
+          mockReq as Request,
+          mockRes as Response,
+          mockNextFunction
+        );
+
+        expect(mockNextFunction).not.toHaveBeenCalled();
+        expect(mockRes.status).toHaveBeenCalledWith(404);
+        expect(mockRes.json).toHaveBeenCalledWith({
+          message: "User not found.",
+        });
+      });
+    });
+
+    describe("next", () => {
+      it("should call next if isSuperAdmin true", async () => {
+        vi.mocked(Admin.findOne).mockResolvedValue({
+          email: "admin@test.com",
+          status: "super-admin",
+        });
+
+        const token = jwt.sign(
+          { isSuperAdmin: true, email: "admin@test.com" },
+          secret
+        );
+
+        mockReq.headers = {
+          authorization: `Bearer ${token}`,
+        };
+
+        await superAdminCheck(
+          mockReq as Request,
+          mockRes as Response,
+          mockNextFunction
+        );
+
+        expect(mockNextFunction).toHaveBeenCalled();
+        expect(mockRes.status).not.toHaveBeenCalledWith(404);
+        expect(mockRes.json).not.toHaveBeenCalledWith();
+      });
     });
   });
 });
